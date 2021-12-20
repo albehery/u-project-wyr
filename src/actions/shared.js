38ba4f302c0d160864efe44513c671b0ceaa94a0
Mohamed Albehery <m@albehery.com>
2021-12-20 18:19:33 +0200

    feat:add question componenet input

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -2,19 +2,19 @@ import { _getUsers, _getQuestions } from '../_DATA'
 import { getQuestions } from './questions';
 import { getUsers } from './users';
 
-  export async function getInitialData() {
-    const [users, questions] = await Promise.all([_getUsers(), _getQuestions()]);
-    return ({
-        users,
-        questions
+async function getInitialData() {
+  const [users, questions] = await Promise.all([_getUsers(), _getQuestions()]);
+  return ({
+    users,
+    questions
+  });
+}
+
+export function handleInitialData() {
+  return dispatch => {
+    return getInitialData().then(({ users, questions }) => {
+      dispatch(getQuestions(questions));
+      dispatch(getUsers(users));
     });
-  }
-  
-  export function handleInitialData() {
-    return dispatch => {
-      return getInitialData().then(({ users, questions }) => {
-        dispatch(getQuestions(questions));
-        dispatch(getUsers(users));
-      });
-    };
-  }
+  };
+}
--- src/components/AddQuestion.js
+++ src/components/AddQuestion.js
@@ -3,8 +3,15 @@ import React from "react";
 class AddQuestion extends React.Component {
     render() {
         return (
-            <div>
-            <h3>Add a new question</h3>
+            <div className='card'>
+                <h3>Add a new question</h3>
+                <form>
+                    <input type="text" name="option1" id="option1" placeholder="enter first option here" />
+                    <br />
+                    <input type="text" name="option2" id="option2" placeholder="enter secode option here" />
+                    <br />
+                    <button type="submit">Submit</button>
+                </form>
             </div>
         )
     }
