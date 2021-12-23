3f7deb87e6aedd46d7f41143f80827b82f9a245c
Mohamed Albehery <m@albehery.com>
2021-12-23 19:16:37 +0200

    feat:save question answer action and reducer

--- src/actions/questions.js
+++ src/actions/questions.js
@@ -1,8 +1,18 @@
 export const GET_QUESTIONS = 'GET_QUESTIONS'
+export const SAVE_QUESTION_ANSWER = 'SAVE_QUESTION_ANSWER'
 
 export function getQuestions(questions) {
     return {
         type: GET_QUESTIONS,
         questions,
     }
+}
+
+export function addAnswerToQuestion(authedUser, qid, answer) {
+  return {
+    type: SAVE_QUESTION_ANSWER,
+    authedUser,
+    qid,
+    answer
+  };
 }
